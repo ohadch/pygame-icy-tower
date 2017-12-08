96ec02857fc3c26093180fc9450d10125ad8d70b
Ohad Chaet <ohadch9518@gmail.com>
2017-12-08 21:06:03 +0200

    Added steps and ability to stand on them

--- main.py
+++ main.py
@@ -66,6 +66,9 @@ class GameWindow:
             self.air_track.player.update()
             game_window.air_track.player.draw()
 
+            for step in self.air_track.steps:
+                step.draw()
+
             self.session.advance_time()
 
             pygame.display.flip()
@@ -113,8 +116,17 @@ class AirTrack:
     def __init__(self, window):
         self.window = window
         self.player = Player(self.window)
+        self.steps_num = 5
         self.steps = []
+        self.draw_steps()
 
+    def draw_steps(self):
+        margins_m = self.window.env.height_m / self.steps_num
+        y_positions_m = range(margins_m, self.window.env.height_m, margins_m)
+        for y_pos_m in y_positions_m:
+            s = Step(self.window)
+            s.center_m.y_m = y_pos_m
+            self.steps.append(s)
 
 # ==================
 # Object Classes
@@ -130,6 +142,7 @@ class Step:
         self.length_m = random.randint(self.min_length_m, self.max_length_m)
         self.center_m = self.rand_pos()
         self.color = THECOLORS['greenyellow']
+        self.vector = Vector(0, 0)
 
     def right_edge(self):
         return Position(self.center_m.x_m + int(self.length_m / 2), self.center_m.y_m)
@@ -139,7 +152,7 @@ class Step:
 
     def rand_pos(self):
         x_m = random.randint(0, self.window.env.width_m - self.length_m)
-        return Position(x_m, 0)
+        return Position(x_m, self.window.env.height_m - 10)
 
     def draw(self):
         pygame.draw.line(self.window.surface, self.color, self.left_edge().coordinates_px(self.window.env.m_to_px_ratio), self.right_edge().coordinates_px(self.window.env.m_to_px_ratio), 3)
@@ -150,12 +163,13 @@ class Player:
     def __init__(self, window):
         self.window = window
         self.radius_m = 1
+        self.padding_m = 1.0
         self.position = Position(self.window.env.width_m / 2, self.window.env.height_m - self.radius_m)
         self.vector = Vector(0, 0)
         self.jump_mps = 70
         self.speed_mps = 70
-        self.jump_phase = "NO"  # "NO" / "UP" / "DOWN"
         self.is_jumping = False
+        self.step = None
 
     def bottom_vertex(self):
         return Position(self.position.x_m, self.position.y_m + self.radius_m)
@@ -188,6 +202,7 @@ class Player:
     def jump(self):
         self.is_jumping = True
         self.vector.y_mps -= self.jump_mps
+        self.step = None
 
     def apply_gravity(self):
         self.vector.y_mps += self.window.env.gravity_mps2 * self.window.session.dt_s
@@ -195,8 +210,17 @@ class Player:
     def is_colliding(self):
         return self.is_penetrating_floor()
 
+    def is_on_step(self, step):
+        return self.vector.y_mps >= 0 and float(step.left_edge().x_m) < self.position.x_m < float(step.right_edge().x_m) and abs(self.bottom_vertex().y_m - step.center_m.y_m) <= self.padding_m
+
+    def stand_on_step(self, step):
+        self.step = step
+        self.position.y_m = self.step.center_m.y_m - self.radius_m
+        self.vector.y_mps = self.step.vector.y_mps
+
     def update(self):
         self.apply_gravity()
+
         if self.is_jumping:
             self.apply_gravity()
             if self.is_colliding() and not self.vector.y_mps < 0:
@@ -205,6 +229,11 @@ class Player:
         else:
             if self.is_penetrating_floor():
                 self.fix_sticky_bottom()
+
+        for step in self.window.air_track.steps:
+            if self.is_on_step(step):
+                self.stand_on_step(step)
+
         self.position.y_m += self.vector.y_mps * self.window.session.dt_s
 
     def draw(self):
